e3dc534a229232e597feffe3e48ebf7d7bf64e6d
discodancer <cryptically@yandex.ru>
2015-05-05 22:58:21 +0300

    Добавил сюда HW по ЧМВ

---
+++ HW5/HW5/Form1.Designer.cs
@@ -0,0 +1,250 @@
+﻿namespace HW5
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Требуется переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Обязательный метод для поддержки конструктора - не изменяйте
+        /// содержимое данного метода при помощи редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.richTextBox10 = new System.Windows.Forms.RichTextBox();
+            this.buttonPassword = new System.Windows.Forms.Button();
+            this.richTextBox9 = new System.Windows.Forms.RichTextBox();
+            this.buttonLogin = new System.Windows.Forms.Button();
+            this.buttonChangeLanguage = new System.Windows.Forms.Button();
+            this.buttonCountry = new System.Windows.Forms.Button();
+            this.buttonName = new System.Windows.Forms.Button();
+            this.buttonState = new System.Windows.Forms.Button();
+            this.buttonSurname = new System.Windows.Forms.Button();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.richTextBox2 = new System.Windows.Forms.RichTextBox();
+            this.richTextBox3 = new System.Windows.Forms.RichTextBox();
+            this.buttonCity = new System.Windows.Forms.Button();
+            this.richTextBox4 = new System.Windows.Forms.RichTextBox();
+            this.richTextBox5 = new System.Windows.Forms.RichTextBox();
+            this.buttonDistrect = new System.Windows.Forms.Button();
+            this.buttonStreet = new System.Windows.Forms.Button();
+            this.richTextBox6 = new System.Windows.Forms.RichTextBox();
+            this.richTextBox7 = new System.Windows.Forms.RichTextBox();
+            this.buttonHome = new System.Windows.Forms.Button();
+            this.richTextBox8 = new System.Windows.Forms.RichTextBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // richTextBox10
+            // 
+            resources.ApplyResources(this.richTextBox10, "richTextBox10");
+            this.richTextBox10.Name = "richTextBox10";
+            // 
+            // buttonPassword
+            // 
+            resources.ApplyResources(this.buttonPassword, "buttonPassword");
+            this.buttonPassword.Name = "buttonPassword";
+            this.buttonPassword.UseVisualStyleBackColor = true;
+            this.buttonPassword.Click += new System.EventHandler(this.buttonPassword_Click);
+            // 
+            // richTextBox9
+            // 
+            resources.ApplyResources(this.richTextBox9, "richTextBox9");
+            this.richTextBox9.Name = "richTextBox9";
+            // 
+            // buttonLogin
+            // 
+            resources.ApplyResources(this.buttonLogin, "buttonLogin");
+            this.buttonLogin.Name = "buttonLogin";
+            this.buttonLogin.UseVisualStyleBackColor = true;
+            this.buttonLogin.Click += new System.EventHandler(this.buttonLogin_Click);
+            // 
+            // buttonChangeLanguage
+            // 
+            resources.ApplyResources(this.buttonChangeLanguage, "buttonChangeLanguage");
+            this.buttonChangeLanguage.Name = "buttonChangeLanguage";
+            this.buttonChangeLanguage.UseVisualStyleBackColor = true;
+            this.buttonChangeLanguage.Click += new System.EventHandler(this.buttonChangeLanguage_Click);
+            // 
+            // buttonCountry
+            // 
+            resources.ApplyResources(this.buttonCountry, "buttonCountry");
+            this.buttonCountry.Name = "buttonCountry";
+            this.buttonCountry.UseVisualStyleBackColor = true;
+            // 
+            // buttonName
+            // 
+            resources.ApplyResources(this.buttonName, "buttonName");
+            this.buttonName.Name = "buttonName";
+            this.buttonName.UseVisualStyleBackColor = true;
+            // 
+            // buttonState
+            // 
+            resources.ApplyResources(this.buttonState, "buttonState");
+            this.buttonState.Name = "buttonState";
+            this.buttonState.UseVisualStyleBackColor = true;
+            // 
+            // buttonSurname
+            // 
+            resources.ApplyResources(this.buttonSurname, "buttonSurname");
+            this.buttonSurname.Name = "buttonSurname";
+            this.buttonSurname.UseVisualStyleBackColor = true;
+            // 
+            // richTextBox1
+            // 
+            resources.ApplyResources(this.richTextBox1, "richTextBox1");
+            this.richTextBox1.Name = "richTextBox1";
+            // 
+            // richTextBox2
+            // 
+            resources.ApplyResources(this.richTextBox2, "richTextBox2");
+            this.richTextBox2.Name = "richTextBox2";
+            // 
+            // richTextBox3
+            // 
+            resources.ApplyResources(this.richTextBox3, "richTextBox3");
+            this.richTextBox3.Name = "richTextBox3";
+            // 
+            // buttonCity
+            // 
+            resources.ApplyResources(this.buttonCity, "buttonCity");
+            this.buttonCity.Name = "buttonCity";
+            this.buttonCity.UseVisualStyleBackColor = true;
+            // 
+            // richTextBox4
+            // 
+            resources.ApplyResources(this.richTextBox4, "richTextBox4");
+            this.richTextBox4.Name = "richTextBox4";
+            // 
+            // richTextBox5
+            // 
+            resources.ApplyResources(this.richTextBox5, "richTextBox5");
+            this.richTextBox5.Name = "richTextBox5";
+            // 
+            // buttonDistrect
+            // 
+            resources.ApplyResources(this.buttonDistrect, "buttonDistrect");
+            this.buttonDistrect.Name = "buttonDistrect";
+            this.buttonDistrect.UseVisualStyleBackColor = true;
+            // 
+            // buttonStreet
+            // 
+            resources.ApplyResources(this.buttonStreet, "buttonStreet");
+            this.buttonStreet.Name = "buttonStreet";
+            this.buttonStreet.UseVisualStyleBackColor = true;
+            // 
+            // richTextBox6
+            // 
+            resources.ApplyResources(this.richTextBox6, "richTextBox6");
+            this.richTextBox6.Name = "richTextBox6";
+            // 
+            // richTextBox7
+            // 
+            resources.ApplyResources(this.richTextBox7, "richTextBox7");
+            this.richTextBox7.Name = "richTextBox7";
+            // 
+            // buttonHome
+            // 
+            resources.ApplyResources(this.buttonHome, "buttonHome");
+            this.buttonHome.Name = "buttonHome";
+            this.buttonHome.UseVisualStyleBackColor = true;
+            // 
+            // richTextBox8
+            // 
+            resources.ApplyResources(this.richTextBox8, "richTextBox8");
+            this.richTextBox8.Name = "richTextBox8";
+            // 
+            // groupBox1
+            // 
+            resources.ApplyResources(this.groupBox1, "groupBox1");
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.TabStop = false;
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.richTextBox4);
+            this.groupBox2.Controls.Add(this.richTextBox3);
+            this.groupBox2.Controls.Add(this.richTextBox8);
+            this.groupBox2.Controls.Add(this.richTextBox2);
+            this.groupBox2.Controls.Add(this.buttonCity);
+            this.groupBox2.Controls.Add(this.buttonSurname);
+            this.groupBox2.Controls.Add(this.buttonState);
+            this.groupBox2.Controls.Add(this.buttonHome);
+            this.groupBox2.Controls.Add(this.richTextBox5);
+            this.groupBox2.Controls.Add(this.richTextBox7);
+            this.groupBox2.Controls.Add(this.buttonName);
+            this.groupBox2.Controls.Add(this.richTextBox6);
+            this.groupBox2.Controls.Add(this.buttonDistrect);
+            this.groupBox2.Controls.Add(this.richTextBox1);
+            this.groupBox2.Controls.Add(this.buttonCountry);
+            this.groupBox2.Controls.Add(this.buttonStreet);
+            resources.ApplyResources(this.groupBox2, "groupBox2");
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.TabStop = false;
+            // 
+            // Form1
+            // 
+            resources.ApplyResources(this, "$this");
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.richTextBox10);
+            this.Controls.Add(this.buttonPassword);
+            this.Controls.Add(this.richTextBox9);
+            this.Controls.Add(this.buttonChangeLanguage);
+            this.Controls.Add(this.buttonLogin);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.groupBox2);
+            this.Name = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox2.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button buttonChangeLanguage;
+        private System.Windows.Forms.Button buttonLogin;
+        private System.Windows.Forms.RichTextBox richTextBox10;
+        private System.Windows.Forms.Button buttonPassword;
+        private System.Windows.Forms.RichTextBox richTextBox9;
+        private System.Windows.Forms.Button buttonCountry;
+        private System.Windows.Forms.Button buttonName;
+        private System.Windows.Forms.Button buttonState;
+        private System.Windows.Forms.Button buttonSurname;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.RichTextBox richTextBox2;
+        private System.Windows.Forms.RichTextBox richTextBox3;
+        private System.Windows.Forms.Button buttonCity;
+        private System.Windows.Forms.RichTextBox richTextBox4;
+        private System.Windows.Forms.RichTextBox richTextBox5;
+        private System.Windows.Forms.Button buttonDistrect;
+        private System.Windows.Forms.Button buttonStreet;
+        private System.Windows.Forms.RichTextBox richTextBox6;
+        private System.Windows.Forms.RichTextBox richTextBox7;
+        private System.Windows.Forms.Button buttonHome;
+        private System.Windows.Forms.RichTextBox richTextBox8;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.GroupBox groupBox2;
+    }
+}
+
---
+++ HW5/HW5/Form1.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Globalization;
+using System.Threading;
+
+namespace HW5
+{
+    public partial class Form1 : Form
+    {
+        private static int count;
+        public Form1()
+        {
+            count = 0;
+            //Thread.CurrentThread.CurrentUICulture = new CultureInfo("ru-RU");
+            InitializeComponent();
+            foreach (Control c in this.Controls)
+            {
+                // убрать рамки
+                Button b = c as Button;
+                if (b != null && b.Name != "buttonChangeLanguage")
+                {
+                    b.FlatStyle = FlatStyle.Flat;
+                    b.FlatAppearance.BorderSize = 0;
+                }
+
+            } 
+        }
+
+        private void buttonChangeLanguage_Click(object sender, EventArgs e)
+        {
+            //button1.FlatAppearance.BorderSize = 0;
+            string newLanguageString = count++ % 2 == 0 ? "ru-RU" : "en-US";
+            var resources = new ComponentResourceManager(typeof(Form1));
+            CultureInfo newCultureInfo = new CultureInfo(newLanguageString);
+            foreach (Control c in this.Controls)
+            {
+               // применяем язык
+               resources.ApplyResources(c, c.Name, newCultureInfo);
+               
+            } 
+
+        }
+
+        private void textBoxLogin_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void buttonLogin_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void buttonPassword_Click(object sender, EventArgs e)
+        {
+
+        }
+
+
+    }
+}
