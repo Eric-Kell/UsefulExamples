99db1f379790d436d5f84c480365b075fa4f40d7
discodancer <cryptically@yandex.ru>
2015-05-05 23:22:49 +0300

    Fixed bug

--- HW5/HW5/Form1.Designer.cs
+++ HW5/HW5/Form1.Designer.cs
@@ -34,25 +34,24 @@
             this.richTextBox9 = new System.Windows.Forms.RichTextBox();
             this.buttonLogin = new System.Windows.Forms.Button();
             this.buttonChangeLanguage = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.buttonStreet = new System.Windows.Forms.Button();
             this.buttonCountry = new System.Windows.Forms.Button();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.buttonDistrect = new System.Windows.Forms.Button();
+            this.richTextBox6 = new System.Windows.Forms.RichTextBox();
             this.buttonName = new System.Windows.Forms.Button();
+            this.richTextBox7 = new System.Windows.Forms.RichTextBox();
+            this.richTextBox5 = new System.Windows.Forms.RichTextBox();
+            this.buttonHome = new System.Windows.Forms.Button();
             this.buttonState = new System.Windows.Forms.Button();
             this.buttonSurname = new System.Windows.Forms.Button();
-            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
-            this.richTextBox2 = new System.Windows.Forms.RichTextBox();
-            this.richTextBox3 = new System.Windows.Forms.RichTextBox();
             this.buttonCity = new System.Windows.Forms.Button();
-            this.richTextBox4 = new System.Windows.Forms.RichTextBox();
-            this.richTextBox5 = new System.Windows.Forms.RichTextBox();
-            this.buttonDistrect = new System.Windows.Forms.Button();
-            this.buttonStreet = new System.Windows.Forms.Button();
-            this.richTextBox6 = new System.Windows.Forms.RichTextBox();
-            this.richTextBox7 = new System.Windows.Forms.RichTextBox();
-            this.buttonHome = new System.Windows.Forms.Button();
+            this.richTextBox2 = new System.Windows.Forms.RichTextBox();
             this.richTextBox8 = new System.Windows.Forms.RichTextBox();
-            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.richTextBox3 = new System.Windows.Forms.RichTextBox();
+            this.richTextBox4 = new System.Windows.Forms.RichTextBox();
             this.groupBox2 = new System.Windows.Forms.GroupBox();
-            this.groupBox2.SuspendLayout();
             this.SuspendLayout();
             // 
             // richTextBox10
@@ -86,17 +85,61 @@
             this.buttonChangeLanguage.UseVisualStyleBackColor = true;
             this.buttonChangeLanguage.Click += new System.EventHandler(this.buttonChangeLanguage_Click);
             // 
+            // groupBox1
+            // 
+            resources.ApplyResources(this.groupBox1, "groupBox1");
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.TabStop = false;
+            // 
+            // buttonStreet
+            // 
+            resources.ApplyResources(this.buttonStreet, "buttonStreet");
+            this.buttonStreet.Name = "buttonStreet";
+            this.buttonStreet.UseVisualStyleBackColor = true;
+            // 
             // buttonCountry
             // 
             resources.ApplyResources(this.buttonCountry, "buttonCountry");
             this.buttonCountry.Name = "buttonCountry";
             this.buttonCountry.UseVisualStyleBackColor = true;
             // 
+            // richTextBox1
+            // 
+            resources.ApplyResources(this.richTextBox1, "richTextBox1");
+            this.richTextBox1.Name = "richTextBox1";
+            // 
+            // buttonDistrect
+            // 
+            resources.ApplyResources(this.buttonDistrect, "buttonDistrect");
+            this.buttonDistrect.Name = "buttonDistrect";
+            this.buttonDistrect.UseVisualStyleBackColor = true;
+            // 
+            // richTextBox6
+            // 
+            resources.ApplyResources(this.richTextBox6, "richTextBox6");
+            this.richTextBox6.Name = "richTextBox6";
+            // 
             // buttonName
             // 
             resources.ApplyResources(this.buttonName, "buttonName");
             this.buttonName.Name = "buttonName";
             this.buttonName.UseVisualStyleBackColor = true;
+            // 
+            // richTextBox7
+            // 
+            resources.ApplyResources(this.richTextBox7, "richTextBox7");
+            this.richTextBox7.Name = "richTextBox7";
+            // 
+            // richTextBox5
+            // 
+            resources.ApplyResources(this.richTextBox5, "richTextBox5");
+            this.richTextBox5.Name = "richTextBox5";
+            // 
+            // buttonHome
+            // 
+            resources.ApplyResources(this.buttonHome, "buttonHome");
+            this.buttonHome.Name = "buttonHome";
+            this.buttonHome.UseVisualStyleBackColor = true;
             // 
             // buttonState
             // 
@@ -110,94 +153,34 @@
             this.buttonSurname.Name = "buttonSurname";
             this.buttonSurname.UseVisualStyleBackColor = true;
             // 
-            // richTextBox1
-            // 
-            resources.ApplyResources(this.richTextBox1, "richTextBox1");
-            this.richTextBox1.Name = "richTextBox1";
-            // 
-            // richTextBox2
-            // 
-            resources.ApplyResources(this.richTextBox2, "richTextBox2");
-            this.richTextBox2.Name = "richTextBox2";
-            // 
-            // richTextBox3
-            // 
-            resources.ApplyResources(this.richTextBox3, "richTextBox3");
-            this.richTextBox3.Name = "richTextBox3";
-            // 
             // buttonCity
             // 
             resources.ApplyResources(this.buttonCity, "buttonCity");
             this.buttonCity.Name = "buttonCity";
             this.buttonCity.UseVisualStyleBackColor = true;
             // 
-            // richTextBox4
+            // richTextBox2
             // 
-            resources.ApplyResources(this.richTextBox4, "richTextBox4");
-            this.richTextBox4.Name = "richTextBox4";
-            // 
-            // richTextBox5
-            // 
-            resources.ApplyResources(this.richTextBox5, "richTextBox5");
-            this.richTextBox5.Name = "richTextBox5";
-            // 
-            // buttonDistrect
-            // 
-            resources.ApplyResources(this.buttonDistrect, "buttonDistrect");
-            this.buttonDistrect.Name = "buttonDistrect";
-            this.buttonDistrect.UseVisualStyleBackColor = true;
-            // 
-            // buttonStreet
-            // 
-            resources.ApplyResources(this.buttonStreet, "buttonStreet");
-            this.buttonStreet.Name = "buttonStreet";
-            this.buttonStreet.UseVisualStyleBackColor = true;
-            // 
-            // richTextBox6
-            // 
-            resources.ApplyResources(this.richTextBox6, "richTextBox6");
-            this.richTextBox6.Name = "richTextBox6";
-            // 
-            // richTextBox7
-            // 
-            resources.ApplyResources(this.richTextBox7, "richTextBox7");
-            this.richTextBox7.Name = "richTextBox7";
-            // 
-            // buttonHome
-            // 
-            resources.ApplyResources(this.buttonHome, "buttonHome");
-            this.buttonHome.Name = "buttonHome";
-            this.buttonHome.UseVisualStyleBackColor = true;
+            resources.ApplyResources(this.richTextBox2, "richTextBox2");
+            this.richTextBox2.Name = "richTextBox2";
             // 
             // richTextBox8
             // 
             resources.ApplyResources(this.richTextBox8, "richTextBox8");
             this.richTextBox8.Name = "richTextBox8";
             // 
-            // groupBox1
+            // richTextBox3
             // 
-            resources.ApplyResources(this.groupBox1, "groupBox1");
-            this.groupBox1.Name = "groupBox1";
-            this.groupBox1.TabStop = false;
+            resources.ApplyResources(this.richTextBox3, "richTextBox3");
+            this.richTextBox3.Name = "richTextBox3";
+            // 
+            // richTextBox4
+            // 
+            resources.ApplyResources(this.richTextBox4, "richTextBox4");
+            this.richTextBox4.Name = "richTextBox4";
             // 
             // groupBox2
             // 
-            this.groupBox2.Controls.Add(this.richTextBox4);
-            this.groupBox2.Controls.Add(this.richTextBox3);
-            this.groupBox2.Controls.Add(this.richTextBox8);
-            this.groupBox2.Controls.Add(this.richTextBox2);
-            this.groupBox2.Controls.Add(this.buttonCity);
-            this.groupBox2.Controls.Add(this.buttonSurname);
-            this.groupBox2.Controls.Add(this.buttonState);
-            this.groupBox2.Controls.Add(this.buttonHome);
-            this.groupBox2.Controls.Add(this.richTextBox5);
-            this.groupBox2.Controls.Add(this.richTextBox7);
-            this.groupBox2.Controls.Add(this.buttonName);
-            this.groupBox2.Controls.Add(this.richTextBox6);
-            this.groupBox2.Controls.Add(this.buttonDistrect);
-            this.groupBox2.Controls.Add(this.richTextBox1);
-            this.groupBox2.Controls.Add(this.buttonCountry);
-            this.groupBox2.Controls.Add(this.buttonStreet);
             resources.ApplyResources(this.groupBox2, "groupBox2");
             this.groupBox2.Name = "groupBox2";
             this.groupBox2.TabStop = false;
@@ -206,16 +189,31 @@
             // 
             resources.ApplyResources(this, "$this");
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.richTextBox4);
+            this.Controls.Add(this.richTextBox3);
             this.Controls.Add(this.richTextBox10);
+            this.Controls.Add(this.richTextBox8);
             this.Controls.Add(this.buttonPassword);
+            this.Controls.Add(this.richTextBox2);
             this.Controls.Add(this.richTextBox9);
+            this.Controls.Add(this.buttonCity);
             this.Controls.Add(this.buttonChangeLanguage);
+            this.Controls.Add(this.buttonSurname);
             this.Controls.Add(this.buttonLogin);
+            this.Controls.Add(this.buttonState);
             this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.buttonHome);
+            this.Controls.Add(this.buttonName);
+            this.Controls.Add(this.richTextBox5);
+            this.Controls.Add(this.buttonStreet);
+            this.Controls.Add(this.richTextBox7);
+            this.Controls.Add(this.buttonCountry);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.richTextBox6);
+            this.Controls.Add(this.buttonDistrect);
             this.Controls.Add(this.groupBox2);
             this.Name = "Form1";
             this.Load += new System.EventHandler(this.Form1_Load);
-            this.groupBox2.ResumeLayout(false);
             this.ResumeLayout(false);
 
         }
@@ -227,23 +225,23 @@
         private System.Windows.Forms.RichTextBox richTextBox10;
         private System.Windows.Forms.Button buttonPassword;
         private System.Windows.Forms.RichTextBox richTextBox9;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button buttonStreet;
         private System.Windows.Forms.Button buttonCountry;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.Button buttonDistrect;
+        private System.Windows.Forms.RichTextBox richTextBox6;
         private System.Windows.Forms.Button buttonName;
+        private System.Windows.Forms.RichTextBox richTextBox7;
+        private System.Windows.Forms.RichTextBox richTextBox5;
+        private System.Windows.Forms.Button buttonHome;
         private System.Windows.Forms.Button buttonState;
         private System.Windows.Forms.Button buttonSurname;
-        private System.Windows.Forms.RichTextBox richTextBox1;
-        private System.Windows.Forms.RichTextBox richTextBox2;
-        private System.Windows.Forms.RichTextBox richTextBox3;
         private System.Windows.Forms.Button buttonCity;
-        private System.Windows.Forms.RichTextBox richTextBox4;
-        private System.Windows.Forms.RichTextBox richTextBox5;
-        private System.Windows.Forms.Button buttonDistrect;
-        private System.Windows.Forms.Button buttonStreet;
-        private System.Windows.Forms.RichTextBox richTextBox6;
-        private System.Windows.Forms.RichTextBox richTextBox7;
-        private System.Windows.Forms.Button buttonHome;
+        private System.Windows.Forms.RichTextBox richTextBox2;
         private System.Windows.Forms.RichTextBox richTextBox8;
-        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.RichTextBox richTextBox3;
+        private System.Windows.Forms.RichTextBox richTextBox4;
         private System.Windows.Forms.GroupBox groupBox2;
     }
 }
