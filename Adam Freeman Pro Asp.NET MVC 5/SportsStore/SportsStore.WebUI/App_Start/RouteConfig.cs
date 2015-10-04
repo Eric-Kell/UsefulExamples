2f86018f08914262681a4dcd583eb36c02637354
Disco Dancer <cryptically@yandex.ru>
2015-10-04 15:08:39 +0300

    Added Categories

--- Adam Freeman Pro Asp.NET MVC 5/SportsStore/SportsStore.WebUI/App_Start/RouteConfig.cs
+++ Adam Freeman Pro Asp.NET MVC 5/SportsStore/SportsStore.WebUI/App_Start/RouteConfig.cs
@@ -1,29 +1,50 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-using System.Web.Mvc;
+﻿using System.Web.Mvc;
 using System.Web.Routing;
 
 namespace SportsStore.WebUI
 {
     public class RouteConfig
     {
-        public static void RegisterRoutes(RouteCollection routes)
-        {
-            routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
+      public static void RegisterRoutes(RouteCollection routes)
+      {
+        routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
 
-          routes.MapRoute(
-            name: null,
-            url: "Page{page}",
-            defaults: new {Controller = "Product", action = "List"}
-            );
+        // name / url / defaults
+        // for / url
+        routes.MapRoute(null, "",
+          new
+          {
+            controller = "Product",
+            action = "List",
+            category = (string) null,
+            page = 1
+          }
+          );
 
-          routes.MapRoute(
-            name: "Default",
-            url: "{controller}/{action}/{id}",
-            defaults: new {controller = "Product", action = "List", id = UrlParameter.Optional}
-            );
-        }
+        // for /Page2 url
+        routes.MapRoute(null, "Page{page}",
+          new
+          {
+            controller = "Product",
+            action = "List",
+            category =
+              (string) null
+          },
+          new {page = @"\d+"}
+          );
+
+        // for /Soccer url
+        routes.MapRoute(null, "{category}",
+          new {controller = "Product", action = "List", page = 1}
+          );
+
+        // for /Soccer/Page2 url
+        routes.MapRoute(null, "{category}/Page{page}",
+          new {controller = "Product", action = "List"},
+          new {page = @"\d+"}
+          );
+
+        routes.MapRoute(null, "{controller}/{action}");
+      }
     }
 }
