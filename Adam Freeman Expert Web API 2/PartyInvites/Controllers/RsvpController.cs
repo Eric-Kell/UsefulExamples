856669ace72300615ee8470a840dee1d2e4d9b88
Vladislaff Evtushenko <starson4587@gmail.com>
2015-10-31 22:37:32 +0300

    дошел до страницы 23

---
+++ Adam Freeman Expert Web API 2/PartyInvites/Controllers/RsvpController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNet.Mvc;
+using PartyInvites.Models;
+
+// For more information on enabling Web API for empty projects, visit http://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace PartyInvites.Controllers
+{
+  [Route("api/[controller]")]
+  public class RsvpController : Controller
+  {
+    [HttpGet]
+    public IEnumerable<GuestResponse> GetAttendees()
+    {
+      return Repository.Responses.Where(x => x.WillAttend == true);
+    }
+
+    [HttpPost]
+    public void PostResponse(GuestResponse response)
+    {
+      if (ModelState.IsValid)
+      {
+        Repository.Add(response);
+      }
+    }
+
+  }
+}
