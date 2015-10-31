a47db0e946d66f3eb9ba35c2b5ec368fac4f5837
Vladislaff Evtushenko <starson4587@gmail.com>
2015-10-31 12:16:51 +0300

    Example new View Components in ASP NET 5

---
+++ Adam Freeman Expert Web API 2/PartyInvites/Controllers/HomeController.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNet.Mvc;
+using PartyInvites.Models;
+
+namespace PartyInvites.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+    public IActionResult Rsvp()
+    {
+      return View();
+    }
+
+
+    [HttpPost]
+    public IActionResult Rsvp(GuestResponse response)
+    {
+      if (ModelState.IsValid)
+      {
+        Repository.Add(response);
+        return View("Thanks", response);
+      }
+      else
+      {
+        return View();
+      }
+    }
+
+    /*
+    A child action method renders inline HTML markup for part of a view instead of rendering a whole complete view.
+    Unavaliable from url
+    */
+
+    // [ChildActionOnly]
+    //public IActionResult Attendees()
+    //{
+    //  return View(Repository.Responses.Where(x => x.WillAttend == true));
+    //}
+
+  }
+}
---
+++ Adam Freeman Expert Web API 2/PartyInvites/ViewComponents/AttendeesViewComponent.cs
@@ -0,0 +1,19 @@
+﻿using System.Linq;
+using Microsoft.AspNet.Mvc;
+using PartyInvites.Models;
+
+/*
+    View components
+    http://www.asp.net/vnext/overview/aspnet-vnext/vc#intro
+*/
+
+namespace PartyInvites.ViewComponents
+{
+  public class AttendeesViewComponent : ViewComponent
+  {
+    public IViewComponentResult Invoke()
+    {
+      return View(Repository.Responses.Where(x => x.WillAttend == true));
+    }
+  }
+}
