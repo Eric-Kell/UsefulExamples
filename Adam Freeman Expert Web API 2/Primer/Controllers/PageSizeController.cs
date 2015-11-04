12905af7375b519592aeb63975da6c4350716f58
Vladislaff Evtushenko <starson4587@gmail.com>
2015-11-04 16:33:52 +0300

    - Продолжаю разбирать пример из книжки

---
+++ Adam Freeman Expert Web API 2/Primer/Controllers/PageSizeController.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections.Generic;
+using System.Diagnostics;
+using System.Net.Http;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.AspNet.Mvc;
+using Primer.Infrastructure;
+using System.Linq;
+
+// For more information on enabling Web API for empty projects, visit http://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace Primer.Controllers
+{
+    [Route("api/[controller]")]
+    public class PageSizeController : Controller, ICustomController
+  {
+
+    private static string TargetUrl = "http://apress.com";
+
+    [HttpGet]
+    public Task<long> GetPageSize(CancellationToken cToken)
+    {
+      return Task<long>.Factory.StartNew(() =>
+      {
+        System.Net.Http.HttpClient wc = new HttpClient();
+        Stopwatch sw = Stopwatch.StartNew();
+        List<long> results = new List<long>();
+        for (int i = 0; i < 10; i++)
+        {
+          if (!cToken.IsCancellationRequested)
+          {
+            Debug.WriteLine("Making Request: {0}", i);
+            var x = wc.GetAsync(TargetUrl);
+            results.Add(i);
+          }
+          else
+          {
+            Debug.WriteLine("Cancelled");
+            return 0;
+          }
+        }
+        Debug.WriteLine("Elapsed ms: {0}", sw.ElapsedMilliseconds);
+        return (long) results.Average();
+      });
+
+
+    }
+
+    public Task PostUrl(string newUrl, CancellationToken cToken)
+    {
+      TargetUrl = newUrl;
+      return Task.FromResult<object>(null);
+    }
+
+  }
+}
---
+++ Adam Freeman Expert Web API 2/Primer/Properties/AssemblyInfo.cs
@@ -0,0 +1,23 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Primer")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Primer")]
+[assembly: AssemblyCopyright("Copyright ©  2015")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("70c61c2b-1c5d-4d2a-a8e7-e3f17af1d0ff")]
