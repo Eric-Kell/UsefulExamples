4321f16a5688e8326904123c8c239ebdf5842138
Vladislaff Evtushenko <starson4587@gmail.com>
2015-11-09 23:15:07 +0300

    - Пример Task возвращает значение

--- Pro Asynchronous Programming with .NET/Tasks/Program.cs
+++ Pro Asynchronous Programming with .NET/Tasks/Program.cs
@@ -1,32 +1,40 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Numerics;
 using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
 
 namespace Tasks
 {
-  public class DataImporter
-  {
-    public void Import(string directory)
-    {
-    }
-  }
 
   class Program
   {
-
     /*
     Суть в том, что Task делается в BackGround и если нету никаких ForeGround, то прога
     завершится не дожидаясь окончания такски из BackGround
     */
 
+    static BigInteger Factorial(BigInteger x)
+    {
+      if (x == 1)
+        return 1;
+      else
+      {
+        return x * Factorial(x - 1);
+      }
+    }
+
     private static void Main(string[] args)
     {
-      var importer = new DataImporter();
-      string importDirectory = @"C:\data";
-      Task.Factory.StartNew(() => importer.Import(importDirectory));
+      BigInteger n = 4900;
+      BigInteger r = 60;
+      Task<BigInteger> part1 = Task.Factory.StartNew<BigInteger>(() => Factorial(n));
+      Task<BigInteger> part2 = Task.Factory.StartNew<BigInteger>(() => Factorial(n - r));
+      Task<BigInteger> part3 = Task.Factory.StartNew<BigInteger>(() => Factorial(r));
+      BigInteger chances = part1.Result / (part2.Result * part3.Result);
+      Console.WriteLine(chances);
     }
 
   }
