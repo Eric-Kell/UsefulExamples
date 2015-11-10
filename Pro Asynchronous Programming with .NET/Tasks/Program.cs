89fa7d75b8a099e0493cae9fa9775e8fdca5d012
Vladislaff Evtushenko <starson4587@gmail.com>
2015-11-10 08:10:50 +0300

    - Пример выполения Task и ожидания ее завершения в основном потоке.

--- Pro Asynchronous Programming with .NET/Tasks/Program.cs
+++ Pro Asynchronous Programming with .NET/Tasks/Program.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Net;
 using System.Numerics;
 using System.Text;
 using System.Threading;
@@ -11,30 +13,31 @@ namespace Tasks
 
   class Program
   {
-    /*
-    Суть в том, что Task делается в BackGround и если нету никаких ForeGround, то прога
-    завершится не дожидаясь окончания такски из BackGround
-    */
-
-    static BigInteger Factorial(BigInteger x)
+    static void Main(string[] args)
     {
-      if (x == 1)
-        return 1;
-      else
+      Task<string> downloadTask = DownloadWebPageAsync("http://xvideos.com");
+      while (!downloadTask.IsCompleted)
       {
-        return x * Factorial(x - 1);
+        Console.Write(".");
+        Thread.Sleep(250);
       }
+      Console.WriteLine(downloadTask.Result);
     }
 
-    private static void Main(string[] args)
+    private static Task<string> DownloadWebPageAsync(string url)
     {
-      BigInteger n = 4900;
-      BigInteger r = 60;
-      Task<BigInteger> part1 = Task.Factory.StartNew<BigInteger>(() => Factorial(n));
-      Task<BigInteger> part2 = Task.Factory.StartNew<BigInteger>(() => Factorial(n - r));
-      Task<BigInteger> part3 = Task.Factory.StartNew<BigInteger>(() => Factorial(r));
-      BigInteger chances = part1.Result / (part2.Result * part3.Result);
-      Console.WriteLine(chances);
+      return Task.Factory.StartNew(() => DownloadWebPage(url));
+    }
+
+    private static string DownloadWebPage(string url)
+    {
+      WebRequest request = WebRequest.Create(url);
+      WebResponse response = request.GetResponse();
+      var reader = new StreamReader(response.GetResponseStream());
+      {
+        // this will return the content of the web page
+        return reader.ReadToEnd();
+      }
     }
 
   }
