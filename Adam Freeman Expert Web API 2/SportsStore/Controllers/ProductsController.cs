9ae5b915d6fef48baa650b45693cbefae7490482
Vladislaff Evtushenko <starson4587@gmail.com>
2015-11-14 19:54:57 +0300

    - Один из вожножный способ сообщения об ошибке в API

---
+++ Adam Freeman Expert Web API 2/SportsStore/Controllers/ProductsController.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Web.Http;
+using SportsStore.Models;
+using System.Threading.Tasks;
+using System.Web;
+
+namespace SportsStore.Controllers
+{
+  public class ProductsController : ApiController
+  {
+    private IRepository Repository { get; set; }
+
+    public ProductsController()
+    {
+      Repository = new ProductRepository();
+    }
+
+    public IEnumerable<Product> GetProducts()
+    {
+      return Repository.Products;
+    }
+
+    public Product GetProduct(int id)
+    {
+      Product result = Repository.Products.FirstOrDefault(p => p.Id == id);
+      if (result == null)
+      {
+        throw new HttpResponseException(HttpStatusCode.BadRequest);
+      }
+      else
+      {
+        return result;
+      }
+    }
+
+    public async Task PostProduct(Product product)
+    {
+      await Repository.SaveProductAsync(product);
+    }
+
+    public async Task DeleteProduct(int id)
+    {
+      await Repository.DeleteProductAsync(id);
+    }
+  }
+}
