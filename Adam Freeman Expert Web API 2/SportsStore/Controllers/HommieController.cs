909f36ae0d94f9e55d74b79f5a5c19478171248d
Vladislaff Evtushenko <starson4587@gmail.com>
2015-11-14 23:05:12 +0300

    - Добрался до страницы 145.

---
+++ Adam Freeman Expert Web API 2/SportsStore/Controllers/HommieController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SportsStore.Controllers
+{
+    public class HommieController : Controller
+    {
+        // GET: Hommie
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
